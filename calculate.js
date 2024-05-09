a9dc2aa8e9b731335dc75210f8b4825c7656b366
Megan L <meganle414@gmail.com>
2024-05-09 13:01:35 -0700

    Updated popup.js functions for readability and added new video tutorial

--- calculate.js
+++ calculate.js
@@ -3,37 +3,93 @@ const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
 const app = express();
-// const cors = require('cors');
-// app.use(cors);
 app.use(express.json());
+
+const scrapeAmazon = async (url) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const html = await page.content();
+  const $ = cheerio.load(html);
+
+  const prices = [];
+  $('span.a-price').each((index, element) => {
+    const priceText = $(element).find('span').first().text().replace('$', '');
+    prices.push(parseFloat(priceText));
+  });
+
+  const imageUrls = [];
+  $('#g-items li img').each((index, element) => {
+    const imageUrl = $(element).attr('src');
+    imageUrls.push(imageUrl);
+  });
+  imageUrls.pop();
+
+  const titles = [];
+  const urls = [];
+  $('#g-items li a').each((index, element) => {
+    const title = $(element).attr('title');
+    const url = $(element).attr('href');
+    if (title && !titles.includes(title)) {
+      titles.push(title);
+    }
+    if (url.includes("/dp/") && !urls.includes(url) && !url.includes("_im")) {
+      urls.push("https://www.amazon.com/" + url);
+    }
+  });
+
+  await browser.close();
+
+  return { prices, imageUrls, titles, urls };
+};
+
+// const scrapeCamelCamelCamel = async (urls) => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+
+//   const historicalPrices = [];
+//   for (const url of urls) {
+//     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
+//     await page.goto(`https://camelcamelcamel.com/`);
+//     // Print the HTML content of the page
+//     console.log(await page.content());
+//     await page.type('#sq', url);
+//     await page.click('button[value="Search"]');
+//     await page.waitForSelector('.grid-x');
+
+//     const html = await page.content();
+//     const $ = cheerio.load(html);
+
+//     const amazonRow = $('tr.pt.amazon').first();
+//     const lowestEver = amazonRow.find('td:nth-child(2)').text().trim();
+    
+//     if (lowestEver === '') {
+//       lowestEver = 0;
+//     }
+
+//     historicalPrices.push(lowestEver);
+//   }
+
+//   await browser.close();
+
+//   return historicalPrices;
+// };
 
 app.post('/calculate', async (req, res) => {
   try {
     const url = req.body.url;
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-
-    // initialize the array and variable for item prices
-    let prices = [];
-    let totalPrice = 0;
-
-    // get the HTML content
-    const html = await page.content();
-
-    // parse the HTML using cheerio
-    const $ = cheerio.load(html);
-
-    $('span.a-price').each((index, element) => {
-      const priceText = $(element).find('span').first().text().replace('$', '');
-      prices.push(parseFloat(priceText));
-    });
-
-    totalPrice = prices.reduce((acc, current) => acc + current, 0);
+    const { prices, imageUrls, titles, urls } = await scrapeAmazon(url);
+    // const historicalPrices = await scrapeCamelCamelCamel(urls);
 
     const result = {
       itemCount: prices.length,
-      totalPrice: totalPrice.toFixed(2),
+      prices,
+      totalPrice: prices.reduce((acc, current) => acc + current, 0).toFixed(2),
+      imageUrls,
+      urls,
+      titles,
+      // historicalPrices,
     };
 
     console.log('Sending response:', result);
--- popup.js
+++ popup.js
@@ -12,7 +12,6 @@ function updateResultContainer() {
     if (url.includes("amazon.com") && (url.includes("/wishlist") || url.includes("/registry") || url.includes("/baby-reg") || url.includes("/registries"))) {
       document.getElementById("result-container").innerText = url;
       chrome.runtime.sendMessage({ action: 'getResults', url })
-      document.getElementById("result-container").innerText = url;
     } else {
       document.getElementById("result-container").innerText = url + " is not an Amazon List";
     }
@@ -31,6 +30,34 @@ function listenForMessages() {
       const result = request.results;
       document.getElementById("item-count-container").innerHTML = `<br><strong>Count: </strong>${result.itemCount}`;
       document.getElementById("total-price-container").innerHTML = `<strong>Total Price: </strong>$${result.totalPrice}`;
+      const itemsContainer = document.getElementById("items-container");
+
+      itemsContainer.innerHTML = ''; // clear the container
+
+      result.imageUrls.forEach((imageUrl, index) => {
+        const title = document.createElement('p');
+        title.innerHTML = result.titles[index] + "<br>";
+        title.style.textAlign = 'center';
+        itemsContainer.append(title);
+        const img = document.createElement('img');
+        img.src = imageUrl;
+        img.alt = result.titles[index]; // set the alt text to the corresponding title
+        img.style.display = 'block';
+        img.style.margin = '0 auto';
+        itemsContainer.appendChild(img);
+        // itemsContainer.append("<br>");
+        const price = document.createElement('p');
+        price.innerHTML = "$" + result.prices[index] + "<br>";
+        price.style.textAlign = 'right';
+        itemsContainer.append(price);
+      });
+      
+      if (result.imageUrls.length !== 0) {
+        document.getElementById("final-item-count-container").innerHTML = `<br><strong>Count: </strong>${result.itemCount}`;
+        document.getElementById("final-total-price-container").innerHTML = `<strong>Total Price: </strong>$${result.totalPrice}`;
+      } else {
+        document.getElementById("final-item-count-container").innerHTML = `<br><strong>Please make sure the list is being shared!</strong>`;
+      }
     }
   }
 );
