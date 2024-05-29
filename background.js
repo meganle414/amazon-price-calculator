fd5b1c0427616546db6a43b549b6c219f02d8b1c
Megan L <meganle414@gmail.com>
2024-05-29 14:35:12 -0700

    Updated port

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'getResults') {
     const url = request.url;
-    fetch('http://localhost:3000/calculate', {
+    // was http://localhost:3000/calculate
+    fetch('https://amazon-price-calculator-7caab1aba55b.herokuapp.com/calculate', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
